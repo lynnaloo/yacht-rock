e29e7b615f4e920f40ee4bb9429e770e49f89cd1
Linda Nichols <lynnaloo@gmail.com>
2015-02-06 20:22:17 +0000

    Test is now passing, should fix CI

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,10 +2,6 @@
 
 "use strict";
 
-// __tests__/control-test.js
-jest.dontMock('../views/components');
-jest.dontMock('sails');
-
 describe('test', function() {
  it('adds 1 + 2 to equal 3', function() {
    expect(1 + 2).toBe(3);
